650bce44bbcd6e3f96192f53e3dc5775cc02638c
sujij <sujij@SUJISIVAKUMAR>
2024-03-05 23:17:54 +0530

    First core java project

---
+++ Corejavamit/src/day1sample2/samplejava2.java
@@ -0,0 +1,20 @@
+package day1sample2;
+
+import java.util.Scanner;
+
+public class samplejava2 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a,b,c;
+		System.out.println("Enter the a and b values:");
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a+b;
+		System.out.println( "addition of a="+a+" and b="+b+" is:" +c);
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Corejavamit/src/day_1javabasic/Day_1samplejava.java
@@ -0,0 +1,15 @@
+package day_1javabasic;
+
+public class Day_1samplejava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		a=120;
+		b=126;
+		c=a+b;
+        System.out.println("Addition of a="+a+" and b="+b+" is :"+c);
+        System.out.println("Hello World");
+	}
+
+}
